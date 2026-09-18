c30000782f0bd81db5efb23f7444415f6c477a5c
Tomy Maulana <stealthflanker93@gmail.com>
2026-09-18 14:38:27 +0700

    refactor(auth): update AuthLayout import to use AuthSplitLayout

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import AuthLayout from '@/layouts/auth/AuthSimpleLayout.vue';
+import AuthLayout from '@/layouts/auth/AuthSplitLayout.vue';
 import { useTrans } from '@/composables/useTrans';
 import type { TextMessageKey } from '@/types';
 const { trans } = useTrans();
